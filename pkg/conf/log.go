94706db77247628f794eafaf28ef2949f885af99
Ando <ando9527@gmail.com>
2019-07-06 10:14:49 +0800

    Add color to logging

--- pkg/conf/log.go
+++ pkg/conf/log.go
@@ -2,6 +2,7 @@ package conf
 
 import (
 	"os"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
@@ -9,6 +10,11 @@ import (
 func InitLogger(level string) {
 	// Setup logger format
 	l, err := logrus.ParseLevel(level)
+	logrus.SetFormatter(&logrus.TextFormatter{
+		ForceColors:     true,
+		FullTimestamp:   true,
+		TimestampFormat: time.RFC822,
+	})
 	if err != nil {
 		logrus.Fatal(err.Error())
 		os.Exit(1)
