9376760e6fa4cc55185627bd09da88122dfc148e
Ando <ando9527@gmail.com>
2019-08-25 17:28:20 +0800

    next test

--- cloud/main.go
+++ cloud/main.go
@@ -12,6 +12,8 @@ func main(){
 	s := NewServer()
 	s.Routes()
 	port := os.Getenv("PORT")
-
+	if port == "" {
+		port = "8080"
+	}
 	logrus.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), s.router))
 }
