0cdc78c5be580e919108e9920e69b73a7246f94f
Ando <ando9527@gmail.com>
2019-08-25 17:11:13 +0800

    assign port env

--- cloud/main.go
+++ cloud/main.go
@@ -11,6 +11,13 @@ import (
 func main(){
 	s := NewServer()
 	s.Routes()
-	port := os.Getenv("APP_PORT")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = os.Getenv("APP_PORT")
+	}
+	if port == "" {
+		port = "8080"
+	}
+
 	logrus.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), s.router))
 }
