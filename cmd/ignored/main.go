fa8d6485869eb5dc455840ac37b68fcae7cb5cfe
Ando <ando9527@gmail.com>
2019-10-03 22:03:28 +0800

    wip

---
+++ cmd/ignored/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"reflect"
+	"strings"
+
+	"github.com/ando9527/poe-live-trader/pkg/db/ignored"
+	"github.com/sirupsen/logrus"
+)
+
+func main(){
+	c:=ignored.NewClient()
+	for{
+
+		fmt.Println("1. Add user into ignored list.")
+		fmt.Println("2. Remove user from ignored list.")
+		fmt.Println("3. Display ignored list.")
+		fmt.Println("4. Quit.")
+		fmt.Printf("What you choice?(1)")
+		s, e := getInput()
+		fmt.Println(s)
+		if e != nil {
+			logrus.Error(e)
+			continue
+		}
+		if s=="2"{
+			fmt.Println("Removing User Name?")
+			n, e := getInput()
+			if e != nil {
+				logrus.Error( e)
+				continue
+			}
+			e = c.Remove(n)
+			if e != nil {
+				logrus.Error("Failed to remove user from list",e)
+				continue
+			}
+			fmt.Printf("Success to remove %s from list\n", n)
+			continue
+		}else if s =="3"{
+			users, e := c.GetAll()
+			if e != nil {
+				logrus.Error("Failed to get list, ",e)
+				continue
+			}
+			for _,v:=range users{
+				fmt.Println(v)
+			}
+			continue
+		}else if s =="4"{
+			os.Exit(0)
+		} else{
+			fmt.Print("Add User Name?")
+			name,e :=getInput()
+			if e != nil {
+				logrus.Error(e)
+				continue
+			}
+			e = c.Add(name)
+			if e != nil {
+				logrus.Error("Failed to add user in to list", e)
+				continue
+			}
+			fmt.Printf("Succes to add %s into list\n", name)
+		}
+
+	}
+
+}
+
+
+func getInput()(s string, e error){
+	reader := bufio.NewReader(os.Stdin)
+	text, e := reader.ReadString('\n')
+	if e != nil {
+		return "",e
+	}
+	s=strings.Replace(text,"\n","", -1)
+	s=strings.Replace(s," ","", -1)
+	fmt.Println(s=="4")
+	fmt.Println(reflect.TypeOf(s))
+	return s, e
+}
